fd6e94f102b6fa2be7755d1a0f029cc1b51e245d
lenovo <lenovo@LAPTOP-HFUKHVHH>
2020-03-16 11:57:48 +0800

    Math中重构了GetResult（）函数，Program中重写了主函数

---
+++ Test2-5/Math.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_5
+{
+    class Math
+    {
+       
+        public void Equals(int num1,int num2)
+        {
+            if (num1 == num2)
+            { Console.WriteLine("两个整数相等"); }
+        }
+      
+        public void GetResult(string fun,int num1,int num2)
+        {
+            int jie = 0;
+            string fu = "";
+            switch (fun)
+            {
+                case "1":
+                    jie = num1 + num2;
+                    fu = "+";
+                    break;
+                case "2":
+                    jie = num1 - num2;
+                    fu = "-";
+                    break;
+                case "3":
+                    jie = num1 * num2;
+                    fu = "*";
+                    break;
+                case "4":
+                    jie = num1 /num2;
+                    fu = "/";
+                    break;
+                default:
+                    Console.WriteLine("输入有误，请重新输入");
+                    string str = Console.ReadLine();
+                    GetResult(str, num1, num2);
+                    return;
+            }
+            Console.WriteLine("{0}{3}{1}={2}", num1, num2, jie, fu);
+        }
+    }
+}
---
+++ Test2-5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_5
+{
+    class Program
+    {
+        static int Cheaknum(string num)
+        {
+            try
+            {
+                int i = int.Parse(num);
+                return i;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("输入有误，请重新输入：");
+                string str = Console.ReadLine();
+                return Cheaknum(str);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("请输入第一个数:");
+            string num1 = Console.ReadLine();
+            int number1 = Program.Cheaknum(num1);
+            Console.WriteLine("请输入第二个数:");
+            string num2 = Console.ReadLine();
+            int number2 = Program.Cheaknum(num2);
+            math.Equals(number1, number2);
+            Console.WriteLine("请选择运算符：1.+ 2. - 3. * 4.%");
+            string fun = Console.ReadLine();
+            math.GetResult(fun, number1, number2);
+            
+        }
+    }
+}
