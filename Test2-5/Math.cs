287ea4a194a4f84740a5d52416ed6fcdaa67e17b
lenovo <lenovo@LAPTOP-HFUKHVHH>
2020-03-16 12:01:21 +0800

    Math重构函数，Program修改主函数

--- Test2-5/Math.cs
+++ Test2-5/Math.cs
@@ -45,5 +45,30 @@ namespace Test2_5
             }
             Console.WriteLine("{0}{3}{1}={2}", num1, num2, jie, fu);
         }
+        public void GetResult(string fun, string num1, string num2)
+        {
+            string jie = " ";
+            string fu = "";
+            switch (fun)
+            {
+                case "1":
+                    jie = num1 + num2;
+                    fu = "+";
+                    break;
+                case "2":
+
+
+                    jie = num1.Replace(num2,"");
+                    fu = "-";
+                    break;           
+                default:
+                    Console.WriteLine("输入有误，请重新输入");
+                    string str = Console.ReadLine();
+                    GetResult(str, num1, num2);
+                    return;
+            }
+            Console.WriteLine("{0}{3}{1}={2}", num1, num2, jie, fu);
+        }
+
     }
 }
--- Test2-5/Program.cs
+++ Test2-5/Program.cs
@@ -24,17 +24,44 @@ namespace Test2_5
         }
         static void Main(string[] args)
         {
-            Math math = new Math();
-            Console.WriteLine("请输入第一个数:");
-            string num1 = Console.ReadLine();
-            int number1 = Program.Cheaknum(num1);
-            Console.WriteLine("请输入第二个数:");
-            string num2 = Console.ReadLine();
-            int number2 = Program.Cheaknum(num2);
-            math.Equals(number1, number2);
-            Console.WriteLine("请选择运算符：1.+ 2. - 3. * 4.%");
-            string fun = Console.ReadLine();
-            math.GetResult(fun, number1, number2);
+           
+            Console.WriteLine("请选择做字符串运算（a）还是数值运算（b）");
+            string s = Console.ReadLine();
+             void xuan(string m)
+            {
+                switch (m)
+                {
+                    case "b":
+                        Math math = new Math();
+                        Console.WriteLine("请输入第一个数:");
+                        string num1 = Console.ReadLine();
+                        int number1 = Program.Cheaknum(num1);
+                        Console.WriteLine("请输入第二个数:");
+                        string num2 = Console.ReadLine();
+                        int number2 = Program.Cheaknum(num2);
+                        math.Equals(number1, number2);
+                        Console.WriteLine("请选择运算符：1.+ 2. - 3. * 4.%");
+                        string fun = Console.ReadLine();
+                        math.GetResult(fun, number1, number2);
+                        break;
+                    case "a":
+                        Math math2 = new Math();
+                        Console.WriteLine("请输入第一个数:");
+                        string num11 = Console.ReadLine();
+                        
+                        Console.WriteLine("请输入第二个数:");
+                        string num22 = Console.ReadLine();
+                        
+                        
+                        Console.WriteLine("请选择运算符：1.+ 2. - ");
+                        string p = Console.ReadLine();
+
+                        math2.GetResult(p, num11, num22);
+                        break;
+                }
+            }
+            xuan(s);
+  
             
         }
     }
